6a81e5d1a06bd4f4a529f0cc465eb4119606a7a5
Cris <crisheight@gmail.com>
2024-08-02 15:25:07 -0500

    init main and java swing class

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Create a new instance of the generateUI class
+        generateUI uiStart = new generateUI();
+        uiStart.createUI();
+    }
+}
---
+++ src/generateUI.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class generateUI {
+
+    public void createUI() {
+        JFrame frame = new JFrame("Hello world!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setVisible(true);
+    }
+
+
+}
