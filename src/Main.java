744b4ec6883c987636f180d339a36f8fe4dacf3d
Cris <crisheight@gmail.com>
2024-08-14 14:50:07 -0500

    Changed object instantiation in place with a lambda to call the UI later.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
         // Create a new instance of the generateUI class
-        generateUI uiStart = new generateUI();
-        uiStart.createUI();
+        SwingUtilities.invokeLater(() -> new generateUI().createUI());
+
     }
 }
