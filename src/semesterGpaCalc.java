7c85a2e0fbe1a4aaf874d48d1fb1c56b19f32b31
Cris <crisheight@gmail.com>
2024-08-17 17:25:32 -0500

    implement the class functions that implement the gpaCalcInterface

--- src/semesterGpaCalc.java
+++ src/semesterGpaCalc.java
@@ -1,32 +1,50 @@
+import java.util.Scanner;
+
 public class semesterGpaCalc implements gpaCalcInterface {
     @Override
-    public double[] getClasses() {
-        return new double[0];
+    public double getClasses() {
+        System.out.println("Enter the number of classes you are taking this semester: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
     }
 
     @Override
     public double[] getCredits() {
-        return new double[0];
+        System.out.println("Enter the number of credits for each class: ");
+        Scanner scanner = new Scanner(System.in);
+        double[] credits = new double[(int) getClasses()];
+
+        for (int i = 0; i < credits.length; i++) {
+            credits[i] = scanner.nextDouble();
+        }
+
+        return credits;
     }
 
     @Override
     public double[] getGrades() {
-        return new double[0];
+        System.out.println("Enter the grades for each class: ");
+        Scanner scanner = new Scanner(System.in);
+        double[] grades = new double[(int) getClasses()];
+
+        for (int i = 0; i < grades.length; i++) {
+            grades[i] = scanner.nextDouble();
+        }
+
+        return grades;
     }
 
     @Override
-    public void calculate(double[] classes, double[] credits, double[] grades) {
+    public void calculate(int classes, double[] credits, double[] grades) {
         double totalCredits = 0;
         double totalGradePoints = 0;
-        double gpa;
 
-        for (int i = 0; i < grades.length; i++) {
+        for (int i = 0; i < classes; i++) {
             totalCredits += credits[i];
-            totalGradePoints += grades[i] * credits[i];
+            totalGradePoints += credits[i] * grades[i];
         }
 
-        gpa = totalGradePoints / totalCredits;
-
-        System.out.println("Semester GPA: " + gpa);
+        double gpa = totalGradePoints / totalCredits;
+        System.out.println("Your semester GPA is: " + gpa);
     }
 }
