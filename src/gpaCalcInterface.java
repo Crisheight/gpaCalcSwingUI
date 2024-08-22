b98982faac950f6f5783df00dc5bef32772470a5
Cris <crisheight@gmail.com>
2024-08-22 16:17:18 -0500

    add a 4.0 Scale converter requirement for Interface

--- src/gpaCalcInterface.java
+++ src/gpaCalcInterface.java
@@ -1,6 +1,7 @@
 public interface gpaCalcInterface {
-    double getClasses();
+    int getClasses();
     double[] getCredits();
     double[] getGrades();
+    double convertTo4Scale(double gpa);
     void calculate(int classes, double[] credits, double[] grades);
 }
