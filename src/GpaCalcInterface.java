f05e1adc2cac76deea765152994cb714e327407a
Cris <crisheight@gmail.com>
2024-09-05 22:22:39 -0500

    Remove the interface (not needed for GUI)

--- src/GpaCalcInterface.java
+++
@@ -1,7 +0,0 @@
-public interface GpaCalcInterface {
-    int getClasses();
-    double[] getCredits();
-    double[] getGrades();
-    double convertTo4Scale(double gpa);
-    void calculate(int classes, double[] credits, double[] grades);
-}
