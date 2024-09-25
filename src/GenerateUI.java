ba14ae5b0e7334df5bcaf8bf0d64163172fd71e9
Cris <crisheight@gmail.com>
2024-09-24 20:20:36 -0500

    Make jpanel private, move options into separate function so that forms can access and repaint homescreen

--- src/GenerateUI.java
+++ src/GenerateUI.java
@@ -3,32 +3,19 @@ import java.awt.*;
 
 public class GenerateUI {
 
+    private JPanel panel;
+
     public void createUI() {
         JFrame frame = new JFrame("GPA Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-        frame.setSize(400, 300);
+        frame.setSize(600, 400);
         frame.setVisible(true);
 
-
-
-        JPanel panel = new JPanel();
+        panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
-        JButton option1Btn = new JButton("Calculate Semester GPA");
-        JButton option2Btn = new JButton("Calculate Cumulative GPA");
-        JButton option3Btn = new JButton("Calculate Future GPA");
-        JButton option4Btn = new JButton("Show Current GPA");
-
-        option1Btn.addActionListener(e -> {
-            SemesterGpaForm semesterGpaForm = new SemesterGpaForm();
-            semesterGpaForm.showForm(panel);
-        });
-
-        panel.add(option1Btn);
-        panel.add(option2Btn);
-        panel.add(option3Btn);
-        panel.add(option4Btn);
+        addOptionButtons();
 
         JPanel panelHolder = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
@@ -40,8 +27,28 @@ public class GenerateUI {
 
         panelHolder.add(panel, gbc);
         frame.add(panelHolder);
-
     }
 
+    public void addOptionButtons() {
+        panel.removeAll();
 
-} // End Class GenerateUI
+        JButton option1Btn = new JButton("Calculate Semester GPA");
+        JButton option2Btn = new JButton("Calculate Cumulative GPA");
+        JButton option3Btn = new JButton("Calculate Future GPA");
+        JButton option4Btn = new JButton("Show Current GPA");
+
+        option1Btn.addActionListener(e -> {
+            SemesterGpaForm semesterGpaForm = new SemesterGpaForm();
+            semesterGpaForm.showForm(panel, this);
+        });
+
+        panel.add(option1Btn);
+        panel.add(option2Btn);
+        panel.add(option3Btn);
+        panel.add(option4Btn);
+
+        panel.revalidate();
+        panel.repaint();
+    }
+
+}
